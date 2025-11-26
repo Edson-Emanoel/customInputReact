f4daf7fcb0e935c3f06d426f942b986d24ee1197
Edson-Emanoel <edson.emanoelofc@gmail.com>
2025-11-26 19:47:42 -0300

    feat: terminei o input

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AddTask from "./components/AddTask";
+
+function App() {
+  return (
+    <div className="tasks-container">
+      <h2>Minhas tarefas</h2>
+
+      <AddTask />
+    </div>
+  );
+}
+
+export default App;
